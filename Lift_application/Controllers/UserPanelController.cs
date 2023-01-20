96008a53107e5716dd3cfdac42741fc248779f09
Семён <@DESKTOP-3O86VSI>
2023-01-20 23:19:12 +0800

    Добавлена страница с создание статей

--- Lift_application/Controllers/UserPanelController.cs
+++ Lift_application/Controllers/UserPanelController.cs
@@ -1,12 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Lift_application.Areas.Identity.Data;
+using Lift_application.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Lift_application.Controllers
 {
     public class UserPanelController : Controller
     {
+        SignInManager<Lift_applicationUser> SignInManager;
+        ArticlesContext db;
+
+        public UserPanelController(SignInManager<Lift_applicationUser> signInManager,ArticlesContext context)
+        {
+            SignInManager = signInManager;
+            db = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            if (SignInManager.IsSignedIn(User))
+            {
+                return View();
+            }
+            else
+            {
+                return Redirect("~/Home/Block");
+            }
         }
+        [HttpGet]
+        public IActionResult Publish()
+        {
+            if (User.IsInRole("admin"))
+            {
+                return View();
+            }
+            else
+            {
+                return Redirect("~/Home/Block");
+            }
+        }
+        [HttpPost]
+
+        public IActionResult Publish(Articles articles)
+        {
+            db.Articles.Add(articles);
+            db.SaveChanges();
+            return Redirect("~/UserPanel/Publish");
+        }
+
     }
 }
--- Lift_application/Models/Articles.cs
+++ Lift_application/Models/Articles.cs
@@ -1,10 +1,13 @@
-﻿namespace Lift_application.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lift_application.Models
 {
     public class Articles
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        [DataType(DataType.MultilineText)]
         public string Text { get; set; }
         public string Author { get; set; }
         public string date { get; set; }
